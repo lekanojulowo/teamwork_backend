f375533f191d61560ae63f5610e2d91a95882b31
Lekan Ojulowo <lekanojulowo@users.noreply.github.com>
2019-11-18 13:05:13 +0100

    Update WelcomeMsg.js

--- queries/WelcomeMsg.js
+++ queries/WelcomeMsg.js
@@ -1,17 +1,18 @@
 // Configuration files
 
 const db = require("../config");
+const msg = 'Welcome to Teamwork API';
 
 // add query functions
 
 // GET index Display Welcome Page
 function welcomeMsg(req, res) {
- res.send("Welcome to Teamwork")
+ res.send({msg});
   }
 
 // GET API Welcome Page
 function welcomeAPIMsg(req, res) {
-  res.send("Welcome to Teamwork API")
+  res.send({msg});
   }
 
 
